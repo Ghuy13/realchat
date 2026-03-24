6302ddc2fe31d3130e36aed3242a416f9fac155a
giahuy130604 <ttuongvy722@gmail.com>
2026-03-24 22:45:29 +0700

    feat: thêm tính năng gửi hình ảnh

--- frontend/src/components/chat/NewGroupChatModal.tsx
+++ frontend/src/components/chat/NewGroupChatModal.tsx
@@ -70,7 +70,7 @@ const NewGroupChatModal = () => {
                 </Button>
             </DialogTrigger>
 
-            <DialogContent className="sm:max-w-[425px] border-none ">
+            <DialogContent className="sm:max-w-106.25 border-none ">
                 <DialogHeader>
                     <DialogTitle className="capitalize">tạo nhóm chat mới</DialogTitle>
                 </DialogHeader>
